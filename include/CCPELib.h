3870d448ce6d6a20cc9fe3752affe64abb7d16b2
gmh5225 <2315157@qq.com>
2022-05-18 14:46:33 +0800

    Update

--- include/CCPELib.h
+++ include/CCPELib.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <linuxpe>
 
 namespace CCPELib {
 
@@ -11,9 +10,16 @@ public:
 
 public:
     bool Load(const char *FileName, bool IsClearDebugData = true);
+    bool IsPEFile32(const char *FileName);
+    bool IsPEFile64(const char *FileName);
 
 private:
-    bool mIsClearDebugData;
+    size_t GetFileSizeByName(const char *FileName);
+    bool ReadFileToMemory(const char *FileName, unsigned char *Buffer, size_t Size);
+    bool WriteMemoryToFile(const char *FileName, unsigned char *Buffer, size_t Size);
+
+private:
+    bool mIsClearDebugData = true;
 };
 
 } // namespace CCPELib
--- src/CCPELib.cpp
+++ src/CCPELib.cpp
@@ -1,4 +1,5 @@
 #include "CCPELib.h"
+#include <linuxpe>
 
 #if ((ULONG_MAX) == (UINT_MAX))
 #    define CCPELibIS32BIT
@@ -6,12 +7,11 @@
 #    define CCPELibIS64BIT
 #endif
 
+#define PE_HEADER_SIZE 0x1000
+
 namespace CCPELib {
 
-PELib::PELib()
-{
-    mIsClearDebugData = true;
-}
+PELib::PELib() {}
 
 PELib::~PELib() {}
 
@@ -25,6 +25,173 @@ PELib::Load(const char *FileName, bool IsClearDebugData /*= true*/)
 
     mIsClearDebugData = IsClearDebugData;
     return true;
+}
+
+bool
+PELib::IsPEFile32(const char *FileName)
+{
+    unsigned char PEHeaderBuffer[PE_HEADER_SIZE] = {0};
+    do
+    {
+        if (!ReadFileToMemory(FileName, PEHeaderBuffer, sizeof(PEHeaderBuffer)))
+        {
+            break;
+        }
+
+        auto DosHeaderPtr = (win::dos_header_t *)PEHeaderBuffer;
+        if (DosHeaderPtr->e_magic != win::DOS_HDR_MAGIC)
+        {
+            break;
+        }
+
+        auto NtHeadersPtr = (win::nt_headers_x86_t *)((unsigned char *)PEHeaderBuffer + DosHeaderPtr->e_lfanew);
+        if (NtHeadersPtr->signature != win::NT_HDR_MAGIC)
+        {
+            break;
+        }
+
+        if (NtHeadersPtr->optional_header.magic == win::OPT_HDR32_MAGIC)
+        {
+            // X32
+            return true;
+        }
+
+    } while (0);
+
+    return false;
+}
+
+bool
+PELib::IsPEFile64(const char *FileName)
+{
+    unsigned char PEHeaderBuffer[PE_HEADER_SIZE] = {0};
+    do
+    {
+        if (!ReadFileToMemory(FileName, PEHeaderBuffer, sizeof(PEHeaderBuffer)))
+        {
+            break;
+        }
+
+        auto DosHeaderPtr = (win::dos_header_t *)PEHeaderBuffer;
+        if (DosHeaderPtr->e_magic != win::DOS_HDR_MAGIC)
+        {
+            break;
+        }
+
+        auto NtHeadersPtr = (win::nt_headers_x64_t *)((unsigned char *)PEHeaderBuffer + DosHeaderPtr->e_lfanew);
+        if (NtHeadersPtr->signature != win::NT_HDR_MAGIC)
+        {
+            break;
+        }
+
+        if (NtHeadersPtr->optional_header.magic == win::OPT_HDR64_MAGIC)
+        {
+            // X64
+            return true;
+        }
+
+    } while (0);
+
+    return false;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//// private function
+size_t
+PELib::GetFileSizeByName(const char *FileName)
+{
+    if (FileName == nullptr)
+    {
+        return 0;
+    }
+
+    size_t FileSize = 0;
+
+    auto PF = fopen(FileName, "rb");
+    if (PF)
+    {
+        fseek(PF, 0, SEEK_END);
+
+        FileSize = (size_t)_ftelli64(PF);
+
+        fclose(PF);
+    }
+
+    return FileSize;
+}
+
+bool
+PELib::ReadFileToMemory(const char *FileName, unsigned char *Buffer, size_t Size)
+{
+    if (FileName == nullptr || Buffer == nullptr || Size == 0)
+    {
+        return false;
+    }
+
+    bool Success = false;
+    FILE *PF = nullptr;
+    do
+    {
+        PF = fopen(FileName, "rb");
+        if (!PF)
+        {
+            break;
+        }
+
+        size_t Read = fread(Buffer, Size, 1, PF);
+        if (Read != Size)
+        {
+            break;
+        }
+
+        Success = true;
+    } while (0);
+
+    if (PF)
+    {
+        fclose(PF);
+    }
+
+    return Success;
+}
+
+bool
+PELib::WriteMemoryToFile(const char *FileName, unsigned char *Buffer, size_t Size)
+{
+    if (FileName == nullptr || Buffer == nullptr || Size == 0)
+    {
+        return false;
+    }
+
+    bool Success = false;
+
+    // first we remove the file
+    remove(FileName);
+
+    FILE *PF = nullptr;
+    do
+    {
+        PF = fopen(FileName, "wb");
+        if (!PF)
+        {
+            break;
+        }
+
+        size_t Write = fwrite(Buffer, Size, 1, PF);
+        if (Write != Size)
+        {
+            break;
+        }
+
+        Success = true;
+    } while (0);
+
+    if (PF)
+    {
+        fclose(PF);
+    }
+
+    return Success;
 }
 
 } // namespace CCPELib
