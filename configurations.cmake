9a42e2229b669047413d9699dac83e9f53f5a641
gmh5225 <2315157@qq.com>
2022-05-19 16:46:04 +0800

    Update

--- configurations.cmake
+++ configurations.cmake
@@ -3,3 +3,13 @@ if(ENABLE_TEST)
 add_definitions(-DENABLE_TEST)
 endif()
 
+# set output binary dir
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Output)
+
+# add source directory macro
+add_definitions(-DCCPELIB_SRC_DIR="${CMAKE_CURRENT_SOURCE_DIR}")
+
+
+
+
+
--- test/test1/test.cpp
+++ test/test1/test.cpp
@@ -7,13 +7,14 @@ void
 test_add_section()
 {
     CCPELib::PELib pelib;
-    pelib.Load("E:\\peshell\\smc-testing\\Output\\testexe-ori.exe");
+
+    pelib.Load(CCPELIB_SRC_DIR "/sample/testexe.exe");
     pelib.AppendSection(".hehe", 0x1000, 0);
     std::vector<unsigned char> PatchValue = {0x90, 0x90};
     auto LastSectionVA = pelib.GetLastSectionVA();
     printf("GetLastSectionVA=%p\n", LastSectionVA);
     pelib.PatchAddressByVA(LastSectionVA, PatchValue);
-    pelib.Save("E:\\peshell\\smc-testing\\Output\\testexe-ori.out.exe");
+    pelib.Save(CCPELIB_SRC_DIR "/sample/testexe.out.exe");
 }
 
 int
