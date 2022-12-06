6eaf55aeb5f0ec5f71fc557d3478ec6a2bc880da
gmh5225 <2315157@qq.com>
2022-12-06 20:27:29 +0800

    Update

--- configurations.cmake
+++ configurations.cmake
@@ -1,13 +1,11 @@
 
-if(ENABLE_TEST)
-add_definitions(-DENABLE_TEST)
-endif()
-
 # set output binary dir
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/Output)
 
 # add source directory macro
 add_definitions(-DCCPELIB_SRC_DIR="${CMAKE_CURRENT_SOURCE_DIR}")
+
+
 
 
 
